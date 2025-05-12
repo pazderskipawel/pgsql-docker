cc61996d3d895e88c61f000675bca33c3d3c29a7
Pawel Pazderski <pawelpazderski@hotmail.com>
2025-05-12 16:15:33 +0200

    fixes, added dashboards config

--- prometheus/conf/dbinit/init.sql
+++ prometheus/conf/dbinit/init.sql
@@ -1,2 +1,2 @@
-CREATE USER postgres_exporter WITH PASSWORD 'pass';
-GRANT pg_monitor TO postgres_exporter;
+CREATE USER "postgres_exporter" WITH PASSWORD 'pass';
+GRANT ALL PRIVILEGES ON DATABASE "test"  TO "postgres_exporter";
